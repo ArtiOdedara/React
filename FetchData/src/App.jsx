a32d6ad26edeabed5a60830cf0bb71b3006ee35e
ArtiOdedara <arti.odedara@bacancy.com>
2023-07-11 21:22:03 +0530

    Fetched data and displayed in a table form

--- FetchData/src/App.jsx
+++ FetchData/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
+import ListData from '../src/Components/ListData'
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <>
-    <div>React App</div>
+      <ListData/>
     </>
   )
 }
---
+++ FetchData/src/Components/ListData.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import '../ListData.css'
+
+function ListData() {
+  const [data, setData] = useState([])
+
+  //Fetch data using axios
+  useEffect(() => {
+    axios.get('https://reqres.in/api/users?page=2')
+    .then(response => setData(response.data.data))
+    .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Avatar</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            data.map((record) => (
+              <tr key={record.id}>
+                <td>{record.email}</td>
+                <td>{record.first_name}</td>
+                <td>{record.last_name}</td>
+                <td><img src={record.avatar} alt={record.avatar} className='avatarImg'/></td>
+                <td>
+                  <button className='bg-success editBtn'>Edit</button> |
+                  <button className='bg-danger deleteBtn'>Delete</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default ListData

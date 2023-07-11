59b8511ba3511f3fe0fdc17c659f5e14b804f7fe
ArtiOdedara <arti.odedara@bacancy.com>
2023-07-11 20:11:37 +0530

    Created react app with bootstrap

---
+++ FetchData/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <>
+    <div>React App</div>
+    </>
+  )
+}
+
+export default App

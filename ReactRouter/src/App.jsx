544f68388b87bbe7c9f5122f74d95f00305c8158
ArtiOdedara <arti.odedara@bacancy.com>
2023-07-13 18:46:09 +0530

    Included bootstrap using npm

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
-      <p>React Router</p>
+      <p className='text-primary'>React Router</p>
     </>
   )
 }
